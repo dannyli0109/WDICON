b7d91335216e889dd481c9aeafc40c68d7dfb083
Danny Li <dannyli0109@gmail.com>
2017-08-08 23:58:13 +1000

    intergrated bootstrap theme on /test route

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@ Rails.application.routes.draw do
   get 'welcome/index'
 
 
-   get '/', to: 'pages#home'
+  get '/', to: 'pages#home'
+
+  get '/test', to: 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post '/login', to: 'pages#login' 
-  delete '/login', to: 'pages#logout' 
+  post '/login', to: 'pages#login'
+  delete '/login', to: 'pages#logout'
 
-  get '/dashboard', to: 'pages#dashboard' 
+  get '/dashboard', to: 'pages#dashboard'
 
   get '/api/users/current', to: 'api/users#current_user'
 
   delete '/login', to: 'pages#home' #Clear the session out and go to login
-  
+
 end
