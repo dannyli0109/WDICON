5aa08b46c84dbe4ebb7f1b5e8a9f53a5b50ff4ce
David McDeavitt <davidmcdeavitt@Davids-MacBook-Air.local>
2017-08-09 15:20:32 +1000

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,101 @@ newUser.name = "David"
 newUser.email = "davidmcdeavitt@gmail.com"
 newUser.password = "money"
 newUser.save
+
+
+newUser = User.new
+newUser.name = "Adrian"
+newUser.email = "adrian.mancuso@gmail.com"
+newUser.password = "money"
+newUser.save
+
+
+newUser = User.new
+newUser.name = "Alex"
+newUser.email = "alexmay@intercept-australia-asia.com.au"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Brett"
+newUser.email = "akabrettjohn@gmail.com"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Danny"
+newUser.email = "dannyli0109@gmail.com"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Kit"
+newUser.email = "kit.perez@gmail.com"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Saloni"
+newUser.email = "saloni.1911@gmail.com"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Sam"
+newUser.email = "samwilsonyoung@gmail.com"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "DT"
+newUser.email = "daniel.tsui@ga.co"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "DT"
+newUser.email = "jdecosta@gmail.com"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Kasun"
+newUser.email = "kasun.maldeni@generalassemb.ly"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Matt"
+newUser.email = "matt.aitken@gmail.com"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Pauliina"
+newUser.email = "pauliina.solanne@generalassemb.ly"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Rachel"
+newUser.email = "rache0.246@gmail.com"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Sabrina"
+newUser.email = "sabrina.swatee@gmail.com"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Stephanie"
+newUser.email = "stephanie.ellul@generalassemb.ly"
+newUser.password = "money"
+newUser.save
+
+newUser = User.new
+newUser.name = "Vanessa"
+newUser.email = "vanessa.e.dohnt@gmail.com"
+newUser.password = "money"
+newUser.save
